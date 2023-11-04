973ff47df9fb6e72d31c1582bf3b03a8fa8f7661
chandan2182 <139637586+chandan2182@users.noreply.github.com>
2023-11-04 12:04:50 +0530

    index.js

--- frontend/src/components/user/index.jsx
+++ frontend/src/components/user/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 const User = () => {
   return (
     <>
-    <
+    <Profile/>
+    <Outlet/>
     </>
   )
 }
 
-export default U
+export default User
